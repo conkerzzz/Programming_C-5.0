40259323405e785cf5795255c542828a4c1ad59c
Erik Martines Sanches <erik.martinessanches@gmail.com>
2014-11-13 19:34:41 +0100

    Made Program and Unit Test from Chapter 1

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace HelloWorld2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
