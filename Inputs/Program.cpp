8fcff325d33a4c95f141824953ee87c7eafcf6e8
Erik Martines Sanches <erik.martinessanches@gmail.com>
2015-01-18 10:14:41 +0100

    Organized the folders

--- Inputs/Program.cpp
+++ Inputs/Program.cpp
@@ -2,9 +2,9 @@
 //On win, use ctrl + z to enter end-of-file from the keyboard! (ctrl +d on unix.)
 
 
-int main(){
+int main() {
 	int sum = 0, value;
-	while (std::cin >> value){
+	while (std::cin >> value) {
 		sum += value;
 	}
 	std::cout << "Sum is " << sum << std::endl;
