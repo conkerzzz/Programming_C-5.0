14666069fd9a3908a0657d3722cff67932192eec
Erik Martines Sanches <erik.martinessanches@gmail.com>
2015-01-18 09:15:17 +0100

    CPPP 1.4.4 Reading an unknown number of inputs.

---
+++ Inputs/Program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//On win, use ctrl + z to enter end-of-file from the keyboard! (ctrl +d on unix.)
+
+
+int main(){
+	int sum = 0, value;
+	while (std::cin >> value){
+		sum += value;
+	}
+	std::cout << "Sum is " << sum << std::endl;
+	return 0;
+}
