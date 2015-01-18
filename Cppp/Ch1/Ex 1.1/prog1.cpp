c4290077591f16e4a3b90dbaa2250ed8d844ce2a
Erik Martines Sanches <erik.martinessanches@gmail.com>
2015-01-18 12:05:07 +0100

    Ex 1.1 and 1.2

---
+++ Cppp/Ch1/Ex 1.1/prog1.cpp
@@ -0,0 +1,8 @@
+/**
+* Testing if program runs correctly.
+* @return 0 if successful
+*/
+
+int main(){
+	return 0;
+}
---
+++ Cppp/Ch1/Ex 1.2/prog1.cpp
@@ -0,0 +1,9 @@
+/**
+* Testing the debugger messages by returning -1, a fail indicator
+* post: The program '[5700] Ex 1.2.exe' has exited with code -1 (0xffffffff).
+* @return -1 on failure
+*/
+
+int main(){
+	return -1;
+}
