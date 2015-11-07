14c81a48d080c005ad05431b14257cfeef0cd852
Erik Martines Sanches <erik.martinessanches@gmail.com>
2015-11-07 18:59:21 +0100

    Improvements to Ex 1.23

--- Cppp/Ch1/Ex1_23/Ex1_23.cpp
+++ Cppp/Ch1/Ex1_23/Ex1_23.cpp
@@ -6,14 +6,24 @@
 #include "Sales_item.h"
 
 int _tmain(int argc, _TCHAR* argv[]){
-	std::cout << "Enter several book objects with same ISBN or Ctrl+Z and Enter to quit." << std::endl;
+	std::cout << "Enter several book objects, Ctrl+Z and Enter to quit, apparently." << std::endl;
 	bool entering = true;
 	Sales_item sum, item;
-	std::cin >> sum;
-	while (std::cin >> item) {
-		sum += item;
+	if (std::cin >> sum){
+		while (std::cin >> item) {
+			if (sum.same_isbn(item)){
+				sum = sum + item;
+			} else {
+				std::cout << sum << std::endl;
+				sum = item;
+			}
+		}
+		std::cout << sum << std::endl;
 	}
-	std::cout << "The sum is: " << sum << std::endl;
+	else {
+		std::cout << "No data." << std::endl;
+		return -1;
+	}
 	return 0;
 }
 
